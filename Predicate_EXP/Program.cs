236666d21021476b4813da116fc6604932d0bcfb
Adinarayana Reddy JC <jc.adi101@gmail.com>
2024-12-23 11:01:10 +0530

    predicate with the Anonymous method

--- Predicate_EXP/Program.cs
+++ Predicate_EXP/Program.cs
@@ -3,9 +3,14 @@
     private static int Result;
     static void Main(string[] args)
     {
-        Predicate<Char> objaction = isNumber;
+        Predicate<Char> objPredicate = isNumber;
         Console.WriteLine("It is Number= " + isNumber('1'));
-        
+        //predicate with the Anonymous method
+        Predicate<char> objPredicate1 = delegate (Char a)
+        {
+            return Char.IsNumber(a) ? true : false;
+        };
+        Console.WriteLine("It is Number= " + objPredicate1('1'));
         Console.ReadLine();
     }
     public static bool isNumber(char a)
