b8390065bb1375b33b85238fb57032ac838b126d
Adinarayana Reddy JC <jc.adi101@gmail.com>
2024-12-23 10:40:50 +0530

    Action Example

---
+++ Action_EXP/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    private static int Result;
+    static void Main(string[] args)
+    {
+        Action<int, int> objaction = Add;
+        objaction(20, 10);
+        Console.WriteLine("Add= " +Result);
+        Console.ReadLine();
+    }
+    public static void Add(int a, int b)
+    {
+        Result= a + b;
+    }
+}
