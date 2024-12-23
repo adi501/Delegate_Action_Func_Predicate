d8caccebe6d12a6b1b14776212a68dfe818449d9
Adinarayana Reddy JC <jc.adi101@gmail.com>
2024-12-23 10:31:36 +0530

    func example

---
+++ Func_Example/Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        Func<int, int, int> objFunc = Add;
+        Console.WriteLine("Add= " + objFunc(20, 10));
+        
+        Console.ReadLine();
+    }
+    public static int Add(int a, int b)
+    {
+        return a + b;
+    }
+}
