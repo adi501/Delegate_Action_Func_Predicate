80185ebfc5e3f25b5688664ff5363f9f8e2ad4f6
Adinarayana Reddy JC <jc.adi101@gmail.com>
2024-12-23 10:56:40 +0530

    Pradicate example

---
+++ Predicate_EXP/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    private static int Result;
+    static void Main(string[] args)
+    {
+        Predicate<Char> objaction = isNumber;
+        Console.WriteLine("It is Number= " + isNumber('1'));
+        
+        Console.ReadLine();
+    }
+    public static bool isNumber(char a)
+    {
+       return Char.IsNumber(a) ? true : false;
+    }
+}
