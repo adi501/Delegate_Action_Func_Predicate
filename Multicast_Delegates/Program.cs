2e561ed2d857ecc9925f10c67d5a49e29e1ac514
Adinarayana Reddy JC <jc.adi101@gmail.com>
2024-12-23 10:17:46 +0530

    Multicast_Delegates

---
+++ Multicast_Delegates/Program.cs
@@ -0,0 +1,32 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        myDelegate objDele = Add;
+        objDele += Sub;
+        objDele += Mul;
+        objDele += Div;
+        objDele(20, 10);
+        Console.ReadLine();
+    }
+    delegate void myDelegate(int a, int b);
+    public static void Add(int a, int b)
+    {
+        Console.WriteLine("Add= " + (a + b));
+    }
+    public static void Sub(int a, int b)
+    {
+        Console.WriteLine("Sub= " + (a - b));
+    }
+    public static void Mul(int a, int b)
+    {
+        Console.WriteLine("Mul= " + (a * b));
+    }
+    public static void Div(int a, int b)
+    {
+        Console.WriteLine("Div= " + (a / b));
+    }
+}
+
+
+
