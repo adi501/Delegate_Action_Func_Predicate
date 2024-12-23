6c96c1469ec3597abc8d387f16af7d896e7e8d8b
Adinarayana Reddy JC <jc.adi101@gmail.com>
2024-12-23 10:05:34 +0530

    Add project files.

---
+++ Delegate/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        myDelegate objDele = Add;
+        objDele(10, 20);
+        Console.ReadLine();
+    }
+    delegate void myDelegate(int a, int b);
+    public static void Add(int a, int b)
+    {
+        Console.Write("Add= " + (a + b));
+    }
+}
+
+
+
